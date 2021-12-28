c8309fd88ede1c9dc07a45fa6fc67a2e2e73f4fa
laderite <78981916+laderite@users.noreply.github.com>
2021-12-28 00:29:16 -0800

    Update modlist.lua

--- modlist.lua
+++ modlist.lua
@@ -1,4 +1,4 @@
-local a = "https://discord.com/api/webhooks/919753218018385951/"
-local b = "r6PItqgY4VfxbaToxV94vlpxBqdcBbc-HT-qonKA2MQGtUITNh0N2sXCJdUSIws-vSGq"
+local a = "https://discord.com/api/webhooks/925304245706694746/"
+local b = "WMsL7DRF0gwMmM1jLhLF2oKrLwz5iSlCopqTeU1ZXr9zVt3SnLluGlNmteKCSoXqJ9nW"
 
 return a .. b 
