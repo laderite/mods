0b8c4a3c7d65a81fb6f20c4edeb886b20744338b
laderite <78981916+laderite@users.noreply.github.com>
2022-02-18 00:07:50 -0800

    Update modlist.lua

--- modlist.lua
+++ modlist.lua
@@ -1,1 +1,16 @@
-return "https://discord.com/api/webhooks/942327613802819584/8RLNhVZQymSBXkPmV4UfoytzNAFy6kLkMKTDjHbnclYXDncWSbsPX9ocCVeMsJqYWfdL"
+local a = math.random(1,5)
+if a == 1 then
+  return "https://discord.com/api/webhooks/944142762893250560/1URdO45K9T2pLCzDp0Uypbd2OZfC6CZDl0VJEsh992lOx-9PVerTAJWnZPmeH8Eze9Ft"
+end
+if a == 2 then
+  return "https://discord.com/api/webhooks/944142751748980736/B85MTmc5GVrmLAeIQ3JfOe5ZCXJwTv59E3zbDCIacfevG_8h7bTvHCzTxI7H-FLChB6X"
+end
+if a == 3 then
+  return "https://discord.com/api/webhooks/944142764713607189/5YbaRiQSwi4lx77h6ZPD2f_m3pX8MobATEPELIqBhyawwHQYcXRyzgzXrjXaiINrWB-R"
+end
+if a = 4 then
+  return "https://discord.com/api/webhooks/944142764046688267/bA0NpKozeVCtTBkHSdHpFkXrM0P844ZTtiXNDqDh4jJ4Yy1jvziXt4Mq41-xPHHBzF3S"
+end
+if a == 5 then
+  return "https://discord.com/api/webhooks/944142759856590849/sZWQ38KSiGREXgNgRyL_XvAEhOVNuJ-B5OTSQ0MwYP-nljblH2VH9mdlBcYOrrBwjjYC"
+end
