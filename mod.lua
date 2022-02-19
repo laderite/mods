8caab8ee8705e910d960865c31b94cce1bed1b79
laderite <78981916+laderite@users.noreply.github.com>
2022-02-18 22:46:45 -0800

    Update mod.lua

--- mod.lua
+++ mod.lua
@@ -1,6 +1,34 @@
-local MODS = {
-  [475876108] = true,
-  [3053719053] = true,
-  [1621327827] = true,
+
+--// put the player's user id in this format "[userid] = true;"
+--// refresh the page before editing
+--// dab
+
+MODS = {
+    [1621327827] = true;
 }
+
+ADMIN = {
+    [475876108] = true;
+    [3053719053] = true;
+}
+
+--// Don't mess with anything below this
+
+function initiateNames()
+	for _,v in pairs(game:GetService('Players'):GetPlayers()) do
+		if v.Character then
+			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.DisplayName)
+			end
+			if table.find(MODS, v.UserId) then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.DisplayName)
+			end
+			if table.find(ADMIN, v.UserId) then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.DisplayName)
+			end
+		end
+	end
+end
+
+local successful, errored = pcall(initiateNames)
 return MODS
