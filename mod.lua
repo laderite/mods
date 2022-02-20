84c339d98c60de57a76b76a4224975284eb5d0f8
laderite <78981916+laderite@users.noreply.github.com>
2022-02-19 16:24:53 -0800

    Update mod.lua

--- mod.lua
+++ mod.lua
@@ -18,13 +18,13 @@ function initiateNames()
 	for _,v in pairs(game:GetService('Players'):GetPlayers()) do
 		if v.Character then
 			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.DisplayName)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
 			end
 			if table.find(MODS, v.UserId) then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.DisplayName)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
 			end
 			if table.find(ADMIN, v.UserId) then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.DisplayName)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
 			end
 		end
 	end
