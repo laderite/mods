e381aef2ac42fa7f9941259ced3491a699998a4c
laderite <78981916+laderite@users.noreply.github.com>
2022-02-21 11:13:35 -0800

    Update modlist.lua

--- modlist.lua
+++ modlist.lua
@@ -1,8 +1,4 @@
-local hooks = {"https://discord.com/api/webhooks/944142751748980736/B85MTmc5GVrmLAeIQ3JfOe5ZCXJwTv59E3zbDCIacfevG_8h7bTvHCzTxI7H-FLChB6X",
-"https://discord.com/api/webhooks/944142762893250560/1URdO45K9T2pLCzDp0Uypbd2OZfC6CZDl0VJEsh992lOx-9PVerTAJWnZPmeH8Eze9Ft",
-"https://discord.com/api/webhooks/944142763954409472/7sHhDEMhCbx4HYOnIaL9JUvBOkaM4iaH_WL5xGRUb75LuqfUOqj1mCfhUnQRS6MwKx8V",
-"https://discord.com/api/webhooks/944142764046688267/bA0NpKozeVCtTBkHSdHpFkXrM0P844ZTtiXNDqDh4jJ4Yy1jvziXt4Mq41-xPHHBzF3S",
-"https://discord.com/api/webhooks/944142764713607189/5YbaRiQSwi4lx77h6ZPD2f_m3pX8MobATEPELIqBhyawwHQYcXRyzgzXrjXaiINrWB-R",
-"https://discord.com/api/webhooks/944749681043439617/spVH6BqvfKtxWeFSOhlm9BglPftFOOspwBvYQUYzU4bDp_gSYlOhfLZohkpcTiQbvYfx",}
+local hooks = {"https://discord.com/api/webhooks/945397777679126580/yW1mbQpggON3mHmLlysEeNRIWKa4CLa9Z5wnGQFD4KZhl9yfm468GQwDGFxowZZDbM-c",
+"https://discord.com/api/webhooks/945397770418810891/GMoCNf1UyTtQtVQ1uHX4Q5CYQctaHQG2o3FqmQexBohaoB7jMRYgSSqiKDU2bNiTLbkG",}
 
 return hooks[math.random(1, #hooks)]
