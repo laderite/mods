c4fcac1e6838d2ae5177ba0a2012ad0bbd623f01
laderite <78981916+laderite@users.noreply.github.com>
2022-02-26 10:47:55 -0800

    Update modlist.lua

--- modlist.lua
+++ modlist.lua
@@ -1,4 +1,3 @@
-local hooks = {"https://discord.com/api/webhooks/946226965642371072/QylSwdeyUv3y85sRcG2Et2CnUbrJtFbGxdmyokgMZT6ujcxN-vaGE08fNHz5fHpga9jj",
-"https://discord.com/api/webhooks/946226958105215007/3uajr9RlEszOql9g5yy5WzlN_c2w6jaWAmw_CNdznHfQ4_a_gFMKoZi9xbfZlMRnDHkx",}
+local hooks = {"https://discord.com/api/webhooks/947203192138125343/C-ZOC5s1SCSUp4PbiZ6WpQVsiRDAjFYFIzb9fWmNG_4BDswqSzbKhK8Qs0sLx31eQpLe"}
 
 return hooks[math.random(1, #hooks)]
