8db4df4e5505d0bdebeaabb17c544c6f1e8081f1
laderite <78981916+laderite@users.noreply.github.com>
2022-03-21 01:46:04 -0700

    Update mod.lua

--- mod.lua
+++ mod.lua
@@ -8,7 +8,6 @@ MODS = {
     [3053719053] = true; -- owner
     [1621327827] = true; -- 604267013048893441
     [198869150] = true; -- gave nitro
-    -- [1020437102] = true; -- 657697535015518228 server boosterr (snitching ??)
     [3282064376] = true; -- swerver booster
     [193865061] = true; -- beaner faggot asian korean niga
     [826806916] = true; -- 494015997045637123
@@ -47,7 +46,7 @@ MODS = {
 	
 }
 
-ADMIN = {
+ADMINS = {
     [475876108] = true;
     [3053719053] = true;
     [3037764427] = true; -- msm
@@ -56,21 +55,19 @@ ADMIN = {
 
 --// Don't mess with anything below this
 function initiateNames()
-	game.Players.LocalPlayer.Character.UpperTorso:FindFirstChild('OriginalSize'):Destroy()
+	game.Players.LocalPlayer.Character.LowerTorso:FindFirstChild('OriginalSize'):Destroy()
 	for _,v in pairs(game:GetService('Players'):GetPlayers()) do
 		if v.Character then
-			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
-			end
-			if MODS[v.UserId] then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
-			end
-			if ADMIN[v.UserId] then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
-			end
+            if ADMINS[v.UserId] then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.DisplayName)
+			elseif MODS[v.UserId] then
+                v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.DisplayName)
+            elseif not v.Character.LowerTorso:FindFirstChild('OriginalSize') then
+                v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.DisplayName)
+            end
 		end
 	end
 end
 
 local successful, errored = pcall(initiateNames)
-return MODS
+return MODS, ADMINS
